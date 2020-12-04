e2fa7650433a7478677a3f36550c0b8ce98c3b9b
goldenwo <68965162+goldenwo@users.noreply.github.com>
2020-12-04 13:36:53 -0500

    Fix misdirect

--- server/server.js
+++ server/server.js
@@ -9,13 +9,9 @@ const LocalStrategy = require('passport-local').Strategy;
 const minicrypt = require('./miniCrypt');
 const yahooFinance = require('yahoo-finance');
 const { quote } = require('yahoo-finance');
-//static pages
-
 
 const mc = new minicrypt();
 
-
-// Session configuration
 let password;
 let secrets;
 let thisSecret;
@@ -26,51 +22,39 @@ if (!process.env.SECRET) {
 		thisSecret = process.env.SECRET;
 	}
 const session = {
-    secret : thisSecret, // set this encryption key in Heroku config (never in GitHub)!
+    secret : thisSecret,
     resave : false,
     saveUninitialized: false
 };
 
-// Passport configuration
 
 const strategy = new LocalStrategy(async (username, pw, done) => {
 	if (!findUser(username)) {
-	    // no such user
 	    return done(null, false, { 'message' : 'Wrong username' });
 	}
 	if (!validatePassword(username, pw)) {
-	    // invalid password
-	    // should disable logins after N messages
-	    // delay return to rate-limit brute-force attacks
-	    await new Promise((r) => setTimeout(r, 500)); // 500ms delay
+	    await new Promise((r) => setTimeout(r, 500));
 	    return done(null, false, { 'message' : 'Wrong password' });
 	}
-	// success!
-	// should create a user object here, associated with a unique identifier
 	return done(null, username);
 });
-
-// App configuration
 
 app.use(expressSession(session));
 passport.use(strategy);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Convert user object to a unique identifier.
 passport.serializeUser((user, done) => {
     done(null, user);
 });
-// Convert a unique identifier to a user object.
 passport.deserializeUser((uid, done) => {
     done(null, uid);
 });
 
-app.use(express.json()); // allow JSON inputs
-app.use(express.urlencoded({'extended' : true})); // allow URLencoded data
+app.use(express.json());
+app.use(express.urlencoded({'extended' : true}));
 app.use(express.static('app'));
 
-//Psql setup
 const pgp = require("pg-promise")({
     connect(client) {
         console.log('Connected to database:', client.connectionParameters.database);
@@ -119,9 +103,6 @@ if (!process.env.USERNAME) {
 	} else {
 		password = process.env.USERNAME;
 	}
-
-		
-	
 
 async function addUser(name, pw, assigned_group) {
 	if (findUser(name)) {
@@ -178,8 +159,6 @@ async function validatePassword(name, pwd) {
 	return mc.check(pwd, passwordInfo.salt, passwordInfo.hash);
 }
 
-// Routes
-
 function checkLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
 	next();
@@ -191,17 +170,17 @@ function checkLoggedIn(req, res, next) {
 app.get('/',
 	checkLoggedIn,
 	(req, res) => {
-	    res.send(JSON.stringify({'username' : res.params.userID}));
+	    res.send(JSON.stringify({'username' : req.params.userID}));
 	});
 
 app.post('/login',
 	 passport.authenticate('local' , {
-	     'successRedirect' : '/index.html',
-	     'failureRedirect' : '/login.html' 
+	     'successRedirect' : '../app/index.html',
+	     'failureRedirect' : '../app/login.html' 
 	 }));
 
 app.get('/login',
-	(req, res) => res.sendFile('login.html',
+	(req, res) => res.sendFile('../app/login.html',
 				   { 'root' : __dirname }));
 
 app.get('/logout', (req, res) => {
@@ -221,7 +200,7 @@ app.post('/register',
 	 });
 
 app.get('/register',
-	(req, res) => res.sendFile('register.html',
+	(req, res) => res.sendFile('../app/register.html',
 				   { 'root' : __dirname }));
 
 app.get('/private',
@@ -275,6 +254,10 @@ app.get("/addPortfolio", async (req, res) => {
 app.get("/stockInfo", async (req, res) => {
 	const result = await quote(req.query.symbol, ['price']);
 	res.send(JSON.stringify({'price': result.price.regularMarketPrice, 'percentchange': result.price.regularMarketChangePercent}));
+});
+
+app.get("*", (req, res) => {
+    res.send("Error");
 });
 
 app.listen(port);
